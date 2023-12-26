d56a590edf9369dddd73f398d7909eaef9afa198
Bartek <bartoszwiadernylodz@gmail.com>
2023-12-26 11:56:50 +0100

    firts commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+int main() {
+    int tab[12];
+ srand(time(NULL));
+
+    for(int i = 0;i<12;i++){
+        tab[i]=rand() % 100 + 1;
+    }
+    for(int i = 0;i<12;i++){
+        cout<<tab[i]<<" ";
+    }
+    return 0;
+}
+
